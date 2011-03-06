c8471f6fd5fd24b5aa5b3620c6988ce95a882574
Daniele Pietrobelli <dp7@sanger.ac.uk>
2011-03-06 15:36:28 +0000

    Setting default route for root (/)

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => "DateTime", :action => "get"
 end
