7dbd35ab600c0d15fcd3fa0a5cd18a297c2ea8af
Daniele Pietrobelli <dp7@sanger.ac.uk>
2011-04-12 20:13:37 +0100

    Adding XCode project to git

---
+++ clients/phonegap/Classes/TutorialPhoneGapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TutorialPhoneGapAppDelegate.h
+//  TutorialPhoneGap
+//
+//  Created by Daniele Pietrobelli on 17/03/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface TutorialPhoneGapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
